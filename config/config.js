0d9486b3bf193e1f419b1a0b44848cea3cf7b3a3
Vicente <vppericone@gmail.com>
2022-04-18 17:34:53 -0700

    add prod url db

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
  "dialect": "postgres"
  },
  "production": {
- "use_env_variable": "DATABASE_URL",
+ "use_env_variable": "postgres://tpioaplangvqpv:b030cc4ac31173063de355484f82d06e6fc2c595bcb3be31256f616f37c03d90@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d2ng6hh11aj6ln",
  "dialect": "postgres"
  }
 }
